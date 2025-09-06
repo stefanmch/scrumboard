2b69dd80c1de15b412d358b74a87c4ed078c7d4d
Stefan Mueller <stefan@stefan-m.net>
2025-09-06 21:36:28 +0200

    refactor: remove eslint-plugin-prettier

--- eslint.config.js
+++ eslint.config.js
@@ -1,18 +1,9 @@
 import eslint from '@eslint/js';
 import tseslint from 'typescript-eslint';
-import prettier from 'eslint-plugin-prettier';
 import eslintConfigPrettier from 'eslint-config-prettier';
 
 export default tseslint.config(
   eslint.configs.recommended,
   ...tseslint.configs.recommended,
-  {
-    plugins: {
-      prettier,
-    },
-    rules: {
-      'prettier/prettier': 'error',
-    },
-  },
   eslintConfigPrettier,
 );
